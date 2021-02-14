5a8fe47ca9988c8409b4f1daad5e1b6d864d534b
k4jar <xsssqlx@gmail.com>
2021-02-15 07:52:06 +0800

    Create a arch-chroot.sh

---
+++ arch-chroot.sh
@@ -0,0 +1,15 @@
+ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+hwclock --systohc
+echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen
+locale-gen
+echo 'LANG=en_US.UTF-8' >> /etc/locale.conf
+echo '[+] Enter your hostname'
+read hostname
+echo ${hostname} >> /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1 localhost" >> /etc/hosts
+echo "127.0.0.1 ${hostname}.localdomain ${hostname}" >> /etc/hosts
+pacman -S intel-ucode grub dhcpcd efibootmgr
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Arch Linux
+grub-mkconfig -o /boot/grub/grub.cfg
+echo "[+] please change your root user password then you can reboot your computer"
